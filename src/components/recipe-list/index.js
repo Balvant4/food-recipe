5b926e5dcca045cf86b540b22992e59e11682d49
Balvant Kushwaha <140886238+Balvant4@users.noreply.github.com>
2024-05-26 10:52:43 +0530

    Update index.js

--- src/components/recipe-list/index.js
+++ src/components/recipe-list/index.js
@@ -9,18 +9,17 @@ export default function RecipeList({ recipeList }) {
       <div className="p-4 mx-auto lg:max-w-6xl md:max-w-4xl sm:max-w-full">
         <h2 className="text-4xl font-bold text-gray-800 mb-12">Recipes</h2>
         <Link href={"/"}>
-          <Button variant="destructive" className=" mb-5">
+          <Button variant="destructive" className="mb-5">
             Go Home
           </Button>
         </Link>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {recipeList && recipeList.length > 0
-            ? recipeList.map((recipe, index) => (
-                <Link key={index} href={`/recipe-list/${index}`}>
+            ? recipeList.map((recipe) => (
+                <Link key={recipe.id} href={`/recipe-list/${recipe.id}`}>
                   <Card>
                     <CardContent className="bg-white rounded-md overflow-hidden shadow-md cursor-pointer hover:scale-[1.1] transition-all">
                       <div className="w-full aspect-w-16 aspect-h-8 lg:h-80">
-                        {/* Replace <img> with <Image> */}
                         <Image
                           src={recipe.image}
                           alt={recipe.name}
